0b7bd2ab0b5c085fcca035f013d9da4989d5477f
Obagade <rotimiobags@gmail.com>
2020-05-06 23:35:27 +0200

    grab the search data

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,32 @@
+var UI = {};
+
+
+UI.handleEnterPress = function() {
+	document.querySelector(".js-search").addEventListener('keypress', function( e ) {
+		if ( e.which === 13 ) {
+			var inputValue = e.target.value;
+			
+			SoundCloudAPI.getTrack(inputValue);
+			
+		}
+	});
+}
+
+UI.handleSubmitClick = function() {
+	document.querySelector(".js-submit").addEventListener('click', function( e ) {
+		var inputValue = document.querySelector(".js-search").value;
+		
+		SoundCloudAPI.getTrack(inputValue);
+	});
+} 
+
+//  set up the search
+UI.handleEnterPress();
+UI.handleSubmitClick();
+
+
+
+
 let SoundCloudAPI = {};
 
 SoundCloudAPI.init = function(){
