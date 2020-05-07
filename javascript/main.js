c881f2d55912df939bfc12b32dec27f53d6bcb00
Obagade <rotimiobags@gmail.com>
2020-05-07 23:48:37 +0200

    added the buttons and cards

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,6 @@
+
+
+
 var UI = {};
 
 
@@ -15,55 +18,209 @@ UI.handleEnterPress = function() {
 UI.handleSubmitClick = function() {
 	document.querySelector(".js-submit").addEventListener('click', function( e ) {
 		var inputValue = document.querySelector(".js-search").value;
-		
+		//onValueRead( inputValue );
+		//console.log(inputValue);
 		SoundCloudAPI.getTrack(inputValue);
 	});
 } 
 
-//  set up the search
+// // set up the search
 UI.handleEnterPress();
 UI.handleSubmitClick();
 
 
 
 
-let SoundCloudAPI = {};
-
-SoundCloudAPI.init = function(){
 
 
-SC.initialize({
-  client_id: 'Enter API here yoo'
-});
 
-}
+//  SEARCH THE SOUNDCLOUD API WITH HARDCODED SEARCH 
+var SoundCloudAPI = {};
 
+SoundCloudAPI.init = function() {
+	SC.initialize({
+		client_id: 'Enter your api here'
+	});
+
+} 	
+
+// // set up soundcloud API
 SoundCloudAPI.init();
 
+
+
+
 SoundCloudAPI.getTrack = function(inputValue) {
+	return SC.get('/tracks/', {
+		// q: "sun kill moon"
+		q: inputValue
+	}).then(function(tracks) {
+  		console.log(tracks);
 
+  		
+  		var searchResult = document.querySelector('.js-search-results');
+			searchResult.innerHTML = "";
 
+  		SoundCloudAPI.renderTrack(tracks, searchResult);
+  		// UI.renderTrack(tracks);
 
-
-SC.get('/tracks', {
-  q: inputValue
-}).then(function(tracks) {
-  console.log(tracks);
-});
-
-}
-
-SoundCloudAPI.getTrack("usher");
-
-SoundCloudAPI.renderTracks = function() {
-  let card = document.createElement('div');
-  card.classList.add("card");
-  
-  let searchResults = document.querySelector(".js-search-results");
-  searchResults.appendChild(card);
-
-
+	});
 }
 
 
-SoundCloudAPI.renderTracks();
+
+
+
+
+
+
+SoundCloudAPI.renderTrack = function( tracks, searchResult ) {
+
+		tracks.forEach(function(track) {
+			// console.log(track.title);
+
+			var card = document.createElement('div');
+			card.classList.add('card');
+
+			// image
+			var imageDiv = document.createElement('div');
+			imageDiv.classList.add('image');
+
+			var image_img = document.createElement('img');
+			image_img.classList.add('image_img');
+			image_img.src = track.artwork_url || 'http://lorempixel.com/100/100/abstract/';
+
+			imageDiv.appendChild( image_img );
+
+			// content
+			var content = document.createElement('div');
+			content.classList.add('content');
+
+			var header = document.createElement('div');
+			header.classList.add('header');
+			header.innerHTML = '<a href="' + track.permalink_url + '" target="_blank">' + track.title + '</a>';
+
+			content.appendChild( header );
+
+			searchResult.appendChild(content);
+
+			// button
+			var button = document.createElement('div');
+				button.setAttribute('data-id', track.id) /////THIS COMES DURING PLAY() !!
+			button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+
+			var icon = document.createElement('i');
+			icon.classList.add('add', 'icon');
+
+			var buttonText = document.createElement('span');
+			buttonText.innerHTML = 'Add to playlist';
+
+			button.appendChild( icon );
+			button.appendChild( buttonText );
+
+				button.addEventListener('click', function() {
+
+						SoundCloudAPI.getEmbed(track.uri);
+						//SoundCloudAPI.getWidget(track.uri);
+
+
+
+				});
+
+			// card
+			card.appendChild( imageDiv );
+			card.appendChild( content );
+			card.appendChild( button );
+
+			searchResult.appendChild( card );
+
+		});	
+}
+
+
+
+
+
+
+
+SoundCloudAPI.getEmbed = function( trackPermalink ) {
+	
+	
+
+	SC.oEmbed(trackPermalink, { auto_play: true }).then(function(oEmbed) {
+	  console.log('oEmbed response: ', oEmbed);
+
+	  var sideBar = document.querySelector(".col-left");
+
+	  // var playListArray = [];
+	  // playListArray.push(oEmbed.html)
+
+	  var box = document.createElement("div");
+	  box.innerHTML = oEmbed.html;
+
+	  sideBar.insertBefore(box, sideBar.firstChild);
+
+	  //set local storage so we can call it after refresh 
+	  localStorage.setItem("key", sideBar.innerHTML);	
+
+	  
+	  // grab the widget object
+		var SCWdiget = SoundCloudAPI.getWidget( embed.childNodes[ 0 ] );
+
+		// bind the finish event to init
+		SCWdiget.bind('finish', function() {
+			alert("FINISHED");
+			
+		});
+		SCWdiget.bind('play', function() {
+			var widgetIndex = Array.from( sidebar.childNodes ).indexOf( embed );
+				
+			Playlist.currentTrack = widgetIndex;
+		});
+
+
+	});
+}
+
+SoundCloudAPI.getWidget = function( embedElement ) {
+	return SC.Widget( embedElement );
+}
+
+
+
+
+  var sideBar = document.querySelector(".col-left");
+  sideBar.innerHTML = localStorage.getItem("key");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
